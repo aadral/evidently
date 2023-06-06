9bf05d83940b97a4c080bc3a4e1ba714fd93d72d
Alexey Dral <aadral@bigdatateam.org>
2023-06-07 05:40:25 +0600

    call pandas unit tests always and once, spark only when available

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,27 @@ from pytest_spark.config import SparkConfigBuilder
 from pytest_spark.util import reduce_logging
 
 
+IS_PYSPARK_AVAILABLE = True
+try:
+    from pyspark.sql import SparkSession
+except ImportError:
+    IS_PYSPARK_AVAILABLE = False
+
+PANDAS_OR_SPARK_PARAMS = ["pandas", "spark"] if IS_PYSPARK_AVAILABLE else ["pandas"]
+
+
+def pytest_collection_modifyitems(config, items):
+    if not IS_PYSPARK_AVAILABLE:
+        skip_spark = pytest.mark.skip(reason="need pyspark to run spark related unit tests")
+        for item in items:
+            if "spark_session" in item.fixturenames:
+                item.add_marker(skip_spark)
+
 # with regard to https://docs.pytest.org/en/stable/deprecations.html#calling-fixtures-directly
 # it is necessary to copy some code from pytest_spark library
 
 
-@pytest.fixture(scope="session", params=["pandas", "spark"])
+@pytest.fixture(scope="session", params=PANDAS_OR_SPARK_PARAMS)
 def _pandas_or_spark_session(request):
     """Internal fixture for SparkSession instance.
 
